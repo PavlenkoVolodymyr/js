fca88ee1f2d1a9c5421a4566d4a6578167a76532
Volodymyr <volodymyrp88@gmail.com>
2022-07-27 15:26:28 +0300

    first commit

---
+++ js/script.jsx
@@ -0,0 +1,114 @@
+'use strict'
+
+console.log('arr' + ' - object')
+console.log(4 + +'5') // унарный
+
+let incr = 10,
+  decr = 10
+
+// ++incr
+// --decr
+
+console.log(++incr)
+console.log(--decr)
+
+console.log(5 % 2)
+
+console.log(2 + 2 * 4 !== 8)
+
+const isChecket = false,
+  isClose = false
+
+console.log(isChecket || !isClose)
+// const category = 'toys'
+
+// console.log(`https://someurl.com/${category}/5`)
+
+// const user = 'Vova'
+// alert(`Hello, ${user}`)
+
+// alert('Hello')
+
+// const result = confirm('Are u here?')
+// console.log(result)
+
+// const answer = prompt('вам есть 18?', '18')
+// console.log(typeof answer)
+
+// const answers = []
+
+// answers[0] = prompt('как ваше имя?', '')
+// answers[1] = prompt('как ваше фамилия?', '')
+// answers[2] = prompt('сколько вам лет?', '')
+
+// console.log(typeof answers)
+// let number = 5.6
+// const leftBorderWidth = 1
+
+// number = 10
+// console.log(number)
+
+// const obj = {
+//   name: 'John',
+//   age: 25,
+//   a: 50,
+//   isMarried: false,
+// }
+
+// console.log(obj.name)
+
+// console.log(name)
+// var name = 'Vova'
+
+// {
+//   var result = 50
+// }
+
+// console.log(result)
+
+// alert(5)
+// ;[].push('a')
+
+// let arr = ['plum.png', 'orange.jpg', 8, 'apple.bmp']
+// console.log(arr[1])
+
+// const arr = ['a', 'b', 'c'] //масив//
+
+// arr[10] = '3456'
+
+// console.log(arr)
+
+// const arrObj = {
+//   a: 'a',
+//   1: 'b',
+//   2: 'c',
+//   abc: {
+//     df: [{}, {}],
+//     def: {},
+//   },
+// }
+
+// const b = 'b'
+
+// arrObj.b = '1234'
+// arrObj['b'] = '1234'
+
+// console.log(arrObj['b'])
+// console.log(arrObj.b)
+
+// const obj = { a: 1, b: 2 } //обьект//
+// const obj = {
+//   Anna: 500,
+//   Alice: 800,
+// }
+
+// const storeName = 'Banana';
+
+// const storeDescription = {
+//     budget: 10000,
+//     employees: ['Vova', 'Julia', 'Sergei'],
+//     products: [
+//         {banana: 300},
+//         {apple: 400}],
+//         open: true
+// }
